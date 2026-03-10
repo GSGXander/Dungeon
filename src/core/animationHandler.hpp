66ae3cf003c02c3340f664fc6971bba5e88fdf9c
GSGXander <61768199+GSGXander@users.noreply.github.com>
2026-03-10 10:12:24 -0400

    Updated Animation Handler
    Should be functional now

--- src/core/animationHandler.hpp
+++ src/core/animationHandler.hpp
@@ -8,13 +8,27 @@ class animationHandler
         float spriteX;                  //sprite left bound
         float spriteY;                  //sprite top bound
         int frameSpeed;                 //how long it takes to go to the next frame
+        int frameCounter;               //current fps frame
 
-        float cellSizeX;                //Size of each cell (image size / number of horizontal cells)
-        float cellSizeY;                //s
-        Texture2D spriteSheet;
+        int cellXMax;
+        float cellSizeX;                //Size of each cell horizontally (image size / number of horizontal cells)
+        float cellSizeY;                //Size of each cell vertically(image size / number of vertical cells)
+        Texture2D spriteSheet; 
     
     public:
-        animationHandler(float sprSizeX, float sprSizeY, const char *sheetLocation);
+        animationHandler(int cellCountX, int cellCountY, const char *sheetLocation);
 
+        void playAnimation(int action);
+
+        void playAnimation(int action, int speed);
+
+        float getCellWidth();
         
+        float getCellHeight();
+
+        float getSpriteX();
+
+        float getSpriteY();
+        
+        Texture2D getSpriteSheet();
 };
